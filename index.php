89ad4dca2447c90f2c2ad5231cab3d2d38cc03ca
Mahadi Hasan <mahadihk@outlook.com>
2021-02-01 23:20:04 +0600

    modify the package section, worked on contact section

--- index.php
+++ index.php
@@ -126,51 +126,34 @@
 
             <div class="col-md-4 col-sm-12">
                 <div class="card mb-3">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                        <img src="https://www.pngitem.com/pimgs/m/60-604793_gold-medal-png-olympic-gold-medal-clipart-transparent.png" class="img-fluid" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Gold Package</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text">$ 1500.00</p>
-                        </div>
-                        </div>
+                    <img src="_resources/images/pkg-gold.png" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">Gold Package</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">$ 1500.00</p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-4 col-sm-12">
                 <div class="card mb-3">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                        <img src="https://i.dlpng.com/static/png/6650051_preview.png" class="img-fluid" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Silver Package</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text">$ 1000.00</p>
-                        </div>
-                        </div>
+                    <img src="_resources/images/pkg-silver.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Silver Package</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">$ 1000.00</p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-4 col-sm-12">
                 <div class="card mb-3">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                        <img src="https://www.pngitem.com/pimgs/m/7-78704_gold-medal-png-transparent-png.png" class="img-fluid" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Bronze Package</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text">$ 500.00</p>
-                        </div>
-                        </div>
+                    <img src="_resources/images/pkg-bronze.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Bronze Package</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">$ 500.00</p>
                     </div>
                 </div>
             </div>
@@ -180,7 +163,7 @@
     <!-- Package End -->
 
     <!-- Register Start -->
-    <div class="container mb-3">
+    <div class="container my-3">
         <div class="row register-block p-5">
             <div class="col-6 mt-3 me-auto">
                 <div class="reg-text text-white p-4">
@@ -195,6 +178,97 @@
         </div>
     </div>
     <!-- Register End -->
+
+    <!-- Contact Start -->
+    <div class="container mb-3">
+
+            <div class="col-12">
+                <div class="p-3 text-center">
+                    <h2 class="display-4">Contact Us</h2>
+                    <hr class="feat-separator">
+                    <p class="fs-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio asperiores incidunt quia nemo 
+                    </p>
+                </div>
+            </div>
+
+        <div class="row p-5">
+
+            <div class="col-md-6 col-sm-12">   
+                <p class="fs-3 text-center">Send a message</p>
+                <form action="">
+                    <div class="row mb-3">
+                        
+                        <div class="col">
+
+                            <div class="form-floating">
+                                <input type="text" class="form-control rounded-0" id="fullName" placeholder="John Doe">
+                                <label for="fullName">Your Name</label>
+                            </div>
+                            
+                        </div>
+
+                        <div class="col">
+
+                            <div class="form-floating">
+                                <input type="email" class="form-control rounded-0" id="emailAddress" placeholder="name@example.com">
+                                <label for="emailAddress">Email address</label>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <select class="form-select rounded-0" id="subject" aria-label="Floating label select">
+                            <option selected>Open this select menu</option>
+                            <option value="1">Inquiry</option>
+                            <option value="2">Complain</option>
+                            <option value="3">General</option>
+                        </select>
+                        <label for="subject">Subject</label>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control rounded-0" placeholder="Leave a comment here" id="feedback" style="height: 100px"></textarea>
+                        <label for="feedback">Message</label>
+                    </div>
+
+                    <button class="btn btn-primary rounded-0">Send</button>
+
+                </form>
+            </div>
+
+            <div class="col-md-6 col-sm-12">  
+                <p class="fs-3 text-center">Come visit us</p>
+                
+                <div id="map"></div>
+
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>    
+                
+                <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script> 
+                <script>
+                    var mymap = L.map('map').setView([23.793392, 90.407781], 14);
+
+                    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FkcmVnIiwiYSI6ImNra210aTNwdTJ1YjUybm1ubG91Ym9vd2kifQ.ce1BHreWT8jrOyIR2Y1uow', 
+                    {
+                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                        maxZoom: 18,
+                        id: 'mapbox/streets-v11',
+                        tileSize: 512,
+                        zoomOffset: -1
+                    }).addTo(mymap);
+
+                    var marker = L.marker([23.793392, 90.407781]).addTo(mymap);
+                </script>  
+
+            
+            </div>
+
+        </div>
+    </div>
+    <!-- Contact End -->
+
 <?php
     require_once "inc/footer.php";
 ?>
